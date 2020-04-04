83ef077eeac7425e5cf4fa8f1e2fbb41a12efa70
Michael Both <both.michael@googlemail.com>
2020-04-04 16:47:24 +0200

    Update download-prebuilts to use respective volume

--- src/download-prebuiltapks.sh
+++ src/download-prebuiltapks.sh
@@ -16,8 +16,8 @@ while read git_url git_branch app_list; do
   [ -z "$git_url" ] && continue
   [ -z "$git_branch" ] && git_branch="master"
   git_name=$(git_base_url "$git_url")
-  mkdir -p "$SRC_DIR/prebuiltapks"
-  pushd "$SRC_DIR/prebuiltapks" &>> "$DEBUG_LOG"
+  mkdir -p "${PREBUILTS_DIR}"
+  pushd "${PREBUILTS_DIR}" &>> "$DEBUG_LOG"
   if [ ! -d "$git_name" ]; then
     git clone --single-branch -b "$git_branch" "$git_url" "$git_name" | tee -a "$DEBUG_LOG"
   else
@@ -30,7 +30,7 @@ while read git_url git_branch app_list; do
   for app in $app_list; do
     if grep "$app" <<<"$CUSTOM_PACKAGES" &> /dev/null; then
       [ -d "prebuilts/prebuiltapks/$app" ] && rm -R "prebuilts/prebuiltapks/$app"
-      rsync -av --delete "$SRC_DIR/prebuiltapks/$git_name/$app" prebuilts/prebuiltapks/ | tee -a "$DEBUG_LOG"
+      rsync -av --delete "${PREBUILTS_DIR}/$git_name/$app" prebuilts/prebuiltapks/ | tee -a "$DEBUG_LOG"
     fi
   done
 done < "$BUILD_SCRIPTS_PATH/prebuiltapks"
