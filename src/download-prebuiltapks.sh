ddd3051c8e33420701c4122ad48fcdb6af3ca4b4
Michael Both <both.michael@googlemail.com>
2020-04-04 14:04:40 +0200

    Fix wrong DEBUG_LOG usage with pushd/popd

--- src/download-prebuiltapks.sh
+++ src/download-prebuiltapks.sh
@@ -10,23 +10,23 @@ function git_base_url() {
   echo $base
 }
 
-pushd "$SRC_DIR/$BRANCH_DIR" | tee -a "$DEBUG_LOG"
+pushd "$SRC_DIR/$BRANCH_DIR" &>> "$DEBUG_LOG"
 mkdir -p prebuilts/prebuiltapks
 while read git_url git_branch app_list; do
   [ -z "$git_url" ] && continue
   [ -z "$git_branch" ] && git_branch="master"
   git_name=$(git_base_url "$git_url")
   mkdir -p "$SRC_DIR/prebuiltapks"
-  pushd "$SRC_DIR/prebuiltapks" | tee -a "$DEBUG_LOG"
+  pushd "$SRC_DIR/prebuiltapks" &>> "$DEBUG_LOG"
   if [ ! -d "$git_name" ]; then
     git clone --single-branch -b "$git_branch" "$git_url" "$git_name" | tee -a "$DEBUG_LOG"
   else
-    pushd "$git_name" | tee -a "$DEBUG_LOG"
+    pushd "$git_name" &>> "$DEBUG_LOG"
     git fetch --all | tee -a "$DEBUG_LOG"
     git checkout "$git_branch" | tee -a "$DEBUG_LOG"
-    popd | tee -a "$DEBUG_LOG"
+    popd &>> "$DEBUG_LOG"
   fi
-  popd | tee -a "$DEBUG_LOG"
+  popd &>> "$DEBUG_LOG"
   for app in $app_list; do
     if grep "$app" <<<"$CUSTOM_PACKAGES" &> /dev/null; then
       [ -d "prebuilts/prebuiltapks/$app" ] && rm -R "prebuilts/prebuiltapks/$app"
@@ -34,4 +34,4 @@ while read git_url git_branch app_list; do
     fi
   done
 done < "$BUILD_SCRIPTS_PATH/prebuiltapks"
-popd | tee -a "$DEBUG_LOG"
+popd &>> "$DEBUG_LOG"
