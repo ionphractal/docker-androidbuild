0e9a1a0498eda8ff7a3259737ed7882e0c34a888
Michael Both <both.michael@googlemail.com>
2020-04-11 16:55:52 +0200

    Write sync output only to DEBUG_LOG

--- src/flavors/microg/download-prebuiltapks.sh
+++ src/flavors/microg/download-prebuiltapks.sh
@@ -20,18 +20,18 @@ while read git_url git_branch app_list; do
   mkdir -p "${PREBUILTS_DIR}"
   pushd "${PREBUILTS_DIR}" &>> "$DEBUG_LOG"
   if [ ! -d "$git_name" ]; then
-    git clone --single-branch -b "$git_branch" "$git_url" "$git_name" | tee -a "$DEBUG_LOG"
+    git clone --single-branch -b "$git_branch" "$git_url" "$git_name" &>> "$DEBUG_LOG"
   else
     pushd "$git_name" &>> "$DEBUG_LOG"
-    git fetch --all | tee -a "$DEBUG_LOG"
-    git checkout "$git_branch" | tee -a "$DEBUG_LOG"
+    git fetch --all &>> "$DEBUG_LOG"
+    git checkout "$git_branch" &>> "$DEBUG_LOG"
     popd &>> "$DEBUG_LOG"
   fi
   popd &>> "$DEBUG_LOG"
   for app in $app_list; do
     if grep "$app" <<<"$CUSTOM_PACKAGES" &> /dev/null; then
       [ -d "prebuilts/prebuiltapks/$app" ] && rm -R "prebuilts/prebuiltapks/$app"
-      rsync -av --delete "${PREBUILTS_DIR}/$git_name/$app" prebuilts/prebuiltapks/ | tee -a "$DEBUG_LOG"
+      rsync -av --delete "${PREBUILTS_DIR}/$git_name/$app" prebuilts/prebuiltapks/ &>> "$DEBUG_LOG"
     fi
   done
 done < "$BUILD_SCRIPTS_PATH/prebuiltapks"
